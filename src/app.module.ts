f2f7e79b25b4443b645c9e15a14c0d68b60c7d64
pavvelpotapov <pavelminsk1979@mail.ru>
2024-11-17 06:10:55 +0300

    application work ok in docker container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import * as dotenv from 'dotenv';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './app.repository';
 import { User } from './user.entity';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
